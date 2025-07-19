565350d68a00ed53c41c10c6985cc97bb588fbb1
RayanBersabal <rayanbersabal@gmail.com>
2025-07-19 18:16:10 +0700

    deploy

--- config/cors.php
+++ config/cors.php
@@ -9,6 +9,7 @@ return [
     'allowed_origins' => [
         'http://localhost:5173',
         'https://santapin-bwfsbxgvdccybtey.southeastasia-01.azurewebsites.net',
+        'https://green-pebble-0905ea900.2.azurestaticapps.net/',
     ],
 
     'allowed_origins_patterns' => [],
